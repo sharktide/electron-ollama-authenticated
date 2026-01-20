a602192481b33f481f9cfccb3a938f8ad2d21cfd
Rihaan Meher <sharktidedev@gmail.com>
2026-01-20 12:11:01 -0500

    Add token param to github-fetch.ts

--- src/github-fetch.ts
+++ src/github-fetch.ts
@@ -1,9 +1,13 @@
-export async function githubFetch(url: string): Promise<Response> {
-    const response = await fetch(url, {
-      headers: {
+export async function githubFetch(url: string, token?: string): Promise<Response> {
+    let headers =
+      {
         Accept: 'application/vnd.github+json',
         'X-GitHub-Api-Version': '2022-11-28',
-      },
+      }
+  
+    if (token) { headers['Authorization'] = `Bearer ${token}`; }
+    const response = await fetch(url, {
+      headers: headers,
     })
 
     if (!response.ok) {
